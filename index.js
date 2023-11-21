5510f2f1f6a884c086b25b4c86b5ecdaf6cd31fc
melissa-underwood <melissanneunderwood@gmail.com>
2023-11-21 11:16:23 -0500

    updated resume

---
+++ index.js
@@ -0,0 +1,8 @@
+function singleCard() {
+    cont = document.getElementById("container");
+    cont.style.top = "50%";
+    //cont.style.webkitTransform = "translateY(-50%)";
+    //cont.style.mstransform = "translateY(-50%)";
+    cont.style.transform = "translateY(-50%)";
+
+}
